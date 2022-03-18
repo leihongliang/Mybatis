41a465bdccb75ae6b278c84426fb25faf68a5d09
leihongliang <1204062841@qq.com>
2022-03-18 22:09:01 +0800

    初始化

--- mybatis02/src/main/java/learn/dao/UserMapper.java
+++ mybatis02/src/main/java/learn/dao/UserMapper.java
@@ -1,5 +1,6 @@
 package learn.dao;
 import learn.pojo.User;
+import org.apache.ibatis.session.RowBounds;
 
 import java.util.List;
 import java.util.Map;
@@ -8,6 +9,7 @@ public interface UserMapper {
 
     List<User> getUserLike(String value);
     User getUserById(int id);
-
+    List<User> getUserByLimit(Map<String, Integer>map);
+    List<User> getUserByRowBounds();
 }
 
--- mybatis02/src/test/java/dao/UserDaoTest.java
+++ mybatis02/src/test/java/dao/UserDaoTest.java
@@ -3,9 +3,12 @@ package dao;
 import learn.dao.UserMapper;
 import learn.pojo.User;
 import learn.utils.MybatisUtils;
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class UserDaoTest {
@@ -20,11 +23,40 @@ public class UserDaoTest {
         sqlSession.close();
     }
 
+    /**
+     * 日志
+     */
     @Test
     public void log4j(){
         logger.info("info 进入log4j");
-
     }
+
+    /**
+     * 分页查询
+     */
+    @Test
+    public void getUserByLimit(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        map.put("startIndex",0);
+        map.put("pageSize",2);
+        List<User> list = mapper.getUserByLimit(map);
+        System.out.println(list);
+        sqlSession.close();
+    }
+    /**
+     * RowBounds分页查询
+     */
+    @Test
+    public void getUserByRowBounds(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        RowBounds rowBounds = new RowBounds(1, 2);
+        List<User> list = sqlSession.selectList("learn.dao.UserMapper.getUserByRowBounds", null, rowBounds);
+        System.out.println(list);
+        sqlSession.close();
+    }
+
 
 
 
