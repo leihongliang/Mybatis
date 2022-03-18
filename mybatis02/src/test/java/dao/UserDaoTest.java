0d88a9bd2533fb03c4e72cf4a70cd8c61722c4d2
leihongliang <1204062841@qq.com>
2022-03-18 21:08:12 +0800

    初始化

--- mybatis02/src/test/java/dao/UserDaoTest.java
+++ mybatis02/src/test/java/dao/UserDaoTest.java
@@ -4,13 +4,12 @@ import learn.dao.UserMapper;
 import learn.pojo.User;
 import learn.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class UserDaoTest {
+    static Logger logger = Logger.getLogger(UserDaoTest.class);
     @Test
     public void getUserLike(){
         SqlSession sqlSession = MybatisUtils.getSqlSession();
@@ -20,5 +19,13 @@ public class UserDaoTest {
         System.out.println(user.toString());
         sqlSession.close();
     }
+
+    @Test
+    public void log4j(){
+        logger.info("info 进入log4j");
+
+    }
+
+
 
 }
